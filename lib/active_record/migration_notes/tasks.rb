e3f3ae27f96b8fd995f41e2cc6a21859f3d9be94
Manuel van Rijn <manuel@manuelvanrijn.nl>
2016-09-20 16:11:31 +0200

    wip

--- lib/active_record/migration_notes/tasks.rb
+++ lib/active_record/migration_notes/tasks.rb
@@ -1,9 +1,16 @@
+require 'rake'
+# unless Rake::Task.task_defined?('db:migrate')
+#   load 'active_record/railties/databases.rake'
+# end
+
 task :migration_notes do
   ActiveRecord::MigrationNotes::Handler.instance.output
 end
 
 %w(db:migrate db:rollback).each do |task|
+  puts 'a'
   Rake::Task[task].enhance do
+    # ActiveRecord::MigrationNotes::Handler.instance.output
     Rake::Task[:migration_notes].invoke
   end
 end
--- lib/activerecord-migration_notes.rb
+++ lib/activerecord-migration_notes.rb
@@ -1,5 +1,8 @@
 require 'active_record'
 require 'active_record/migration_notes/migration'
 require 'active_record/migration_notes/handler'
-require 'active_record/migration_notes/railtie' if defined? Rails
+
+ActiveSupport.on_load(:active_record) do
+  require 'active_record/migration_notes/tasks'
+end
 require 'active_record/migration_notes/version'
